777ef417f1a3901f75d296b16323a051942cc926
Jumakri Ridho Fauzi <RidhoInfinitec@INFINITECs-MacBook-Pro.local>
2023-10-05 17:06:43 +0700

    [ridho] - create state management for chart dashboard - wip

--- redux/redcuers.ts
+++ redux/redcuers.ts
@@ -4,6 +4,8 @@ import userReducers from "./features/user/userReducers";
 import rfidReducers from "./features/rfid/rfidReducers";
 import rfidLogReducers from "./features/rfid-log/rfidLogReducers";
 import vehicleTypeReducers from "./features/vehicleType/vehicleTypeReducers";
+import weeklyReducers from "./features/dashboard/weeklyReducers";
+import monthlyReducers from "./features/dashboard/monthlyReducers";
 
 export const combinedReducer = combineReducers({
   //All reducer
@@ -16,4 +18,8 @@ export const combinedReducer = combineReducers({
   rfidLogManagement: rfidLogReducers,
   // vehicle-type
   vehicleTypeManagement: vehicleTypeReducers,
+  // chart-weekly
+  weeklyManagement: weeklyReducers,
+  // chart-monthly
+  monthlyManagement: monthlyReducers,
 });
